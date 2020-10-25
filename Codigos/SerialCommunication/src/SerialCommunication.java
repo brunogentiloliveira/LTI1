c70dec9f21306a64ab88f4fb557e8f83c5c44131
josebravo80132 <51756941+josebravo80132@users.noreply.github.com>
2020-10-25 16:19:28 +0000

    Update para enviar imagens

--- Codigos/SerialCommunication/src/SerialCommunication.java
+++ Codigos/SerialCommunication/src/SerialCommunication.java
@@ -1,25 +1,70 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
+
+import com.sun.xml.internal.messaging.saaj.util.Base64;
 import jssc.SerialPort;
 import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class SerialCommunication{
-    public static void main(String[] args) {
-        SerialPort serialport;
-        serialport = new SerialPort("/dev/cu.usbserial-14230");
-        try{
-            serialport.openPort();
-            serialport.setParams(9600,8,1,0);
+    public static void main(String[] args) throws SerialPortException, IOException {
+        String formato = "";
+        String[] portlist = SerialPortList.getPortNames();
 
-            Scanner reader = new Scanner(System.in);
+        System.out.println("Selecione o tipo de ficheiro a enviar: \n 0 - Sair \n 1 - Ficheiro de Texto(.txt) \n 2 - Ficheiro de Imagem(.jpg)");
+        System.out.print("Opção escolhida: ");
+        Scanner userInput = new Scanner(System.in);
+        int choice = userInput.nextInt();
 
-            if (reader.hasNext()){
-                String buf = reader.next();
-                char[] buf2 = buf.toCharArray();
-                serialport.writeBytes(buf.getBytes());
-                System.out.println(buf.getBytes() + "<= sent!");
-            }
-        }catch(SerialPortException ex){
-            System.out.println(ex);
+        switch (choice){
+            case 0:
+                System.exit(0);
+                break;
+            case 1:            
+                formato = ".txt";
+                sendTextFile(formato, portlist[1]);
+                break;
+            case 2:
+                formato = ".png";
+                sendImageFile(formato, portlist[1]);
+                break;
         }
+
+    }
+    public static void sendTextFile(String formato, String portname) throws SerialPortException, IOException {
+        SerialPort serialPort = new SerialPort(portname);
+        serialPort.openPort();
+        serialPort.setParams(9600,8,1,0);
+
+        byte[] buff = Files.readAllBytes(Paths.get("/home/moutinho/Desktop/teste1.txt"));
+        serialPort.writeBytes(buff);
+
+        System.out.println("Ficheiro enviado");
+    }
+    public static void sendImageFile(String formato, String portname) throws SerialPortException, IOException {
+        SerialPort serialPort = new SerialPort(portname);
+        serialPort.openPort();
+        serialPort.setParams(9600,8,1,0);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        BufferedImage img;
+        img = ImageIO.read(new File("/home/moutinho/Desktop/imgToSend.png"));
+        ImageIO.write(img, "png", baos);
+        byte[] buff = baos.toByteArray();
+        serialPort.writeBytes(buff);
+
+
     }
 }

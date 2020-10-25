39ef0119f192d57e9d74c11ec8ab824a069708ad
josebravo80132 <51756941+josebravo80132@users.noreply.github.com>
2020-10-25 11:32:07 +0000

    Transferencia .txt funcional

--- Codigos/SerialC/src/demo.java
+++ Codigos/SerialC/src/demo.java
@@ -3,14 +3,12 @@ import jssc.SerialPortEvent;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
 
-import javax.swing.text.html.parser.Parser;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class demo { //Recebe Strings da porta serie, enviadas pelo arduino
 
@@ -18,11 +16,9 @@ public class demo { //Recebe Strings da porta serie, enviadas pelo arduino
 
     public  void connect(String portname) { //le da porta c/ name = 'portname'
 
-
         SerialPort port = new SerialPort(portname);
         try {
             port.openPort();
-
             port.setParams(
 
                     SerialPort.BAUDRATE_9600,
@@ -31,24 +27,23 @@ public class demo { //Recebe Strings da porta serie, enviadas pelo arduino
                     SerialPort.PARITY_NONE
 
             );
-
             port.addEventListener((SerialPortEvent event)->{
 
                 if(event.isRXCHAR()) { //ver event.isRXCHAR () //Definir tempo á espera de receber da serial Port ****
 
                         try {
-
                             buffer = port.readBytes();
 
                             String s = new String(buffer, StandardCharsets.UTF_8);
-                        
                             System.out.print(s);
-                            Files.deleteIfExists(Paths.get("/home/moutinho/Desktop/teste.txt")); //Falta so meter isto mais bonito e declarar uma variavel para o PATH
-                            Files.createFile(Paths.get("/home/moutinho/Desktop/teste.txt"));
-                            Files.write(Paths.get("/home/moutinho/Desktop/teste.txt"), s.getBytes(),StandardOpenOption.APPEND);
 
-
-
+                            if(Files.exists(Paths.get("/home/moutinho/Desktop/teste.txt"))){
+                                //Files.write(Paths.get("/home/moutinho/Desktop/teste.txt"), "".getBytes(),StandardOpenOption.WRITE);
+                                Files.write(Paths.get("/home/moutinho/Desktop/teste.txt"), s.getBytes(),StandardOpenOption.APPEND);
+                            }else{
+                                Files.createFile(Paths.get("/home/moutinho/Desktop/teste.txt"));
+                                Files.write(Paths.get("/home/moutinho/Desktop/teste.txt"), s.getBytes(),StandardOpenOption.APPEND);
+                            }
 
                         } catch (SerialPortException | IOException e) {
                             // TODO Auto-generated catch block
@@ -81,7 +76,7 @@ public class demo { //Recebe Strings da porta serie, enviadas pelo arduino
         }
 
         demo obj = new demo();
-        obj.connect("/dev/tnt0");
+        obj.connect(portlist[0]);
 
 
 
@@ -89,4 +84,3 @@ public class demo { //Recebe Strings da porta serie, enviadas pelo arduino
     }
 
 }
-

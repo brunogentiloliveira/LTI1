88633517a0496af16fc7e9defe9ecf123ca2912b
josebravo80132 <51756941+josebravo80132@users.noreply.github.com>
2021-02-06 23:48:32 +0000

    Update Chat.java

--- ApresentacaoFinal/Java/Chat.java
+++ ApresentacaoFinal/Java/Chat.java
@@ -1,16 +1,12 @@
 import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
-import org.w3c.dom.Text;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.plaf.metal.MetalBorders;
-import javax.swing.text.JTextComponent;
+
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,14 +17,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
+
 
 public class Chat {
     public static SerialPort serialPort;
-    public static JFrame chatFrame; public static JTextArea ta; public static JTextField tf_message;
+    public static JFrame chatFrame; public static JTextArea ta, ta2; public static JTextField tf_message;
     public static JLabel btn_anexo; public static JLabel btn_send;
     public static String userDirectory = Paths.get("").toAbsolutePath().toString();
     public static boolean fileReceiving = false;
@@ -48,21 +42,22 @@ public class Chat {
     }
     public void getChatUI(){
 
-        ImageIcon imageIcon = new ImageIcon("/home/moutinho/Transferências/add.png"); Image image = imageIcon.getImage(); // transform it
-        Image newimg = image.getScaledInstance(25, 25,  java.awt.Image.SCALE_SMOOTH); imageIcon = new ImageIcon(newimg);  // transform it back
+        ImageIcon imageIcon = new ImageIcon("/home/moutinho/Transferências/plus-sign.png"); Image image = imageIcon.getImage();
+        Image newimg = image.getScaledInstance(25, 25,  java.awt.Image.SCALE_SMOOTH); imageIcon = new ImageIcon(newimg);
 
-        ImageIcon imageIcon2 = new ImageIcon("/home/moutinho/Transferências/send.png"); Image imageX = imageIcon2.getImage(); // transform it
-        Image newimg2 = imageX.getScaledInstance(25, 25,  java.awt.Image.SCALE_SMOOTH); imageIcon2 = new ImageIcon(newimg2);  // transform it back
+        ImageIcon imageIcon2 = new ImageIcon("/home/moutinho/Transferências/iconfinder_next_293276.png"); Image imageX = imageIcon2.getImage();
+        Image newimg2 = imageX.getScaledInstance(30, 30,  java.awt.Image.SCALE_SMOOTH); imageIcon2 = new ImageIcon(newimg2);
 
 
         chatFrame = new JFrame("Chat-Grupo4-"+serialPort.getSystemPortName());  chatFrame.setSize(800, 490);
         chatFrame.setLocationRelativeTo(null);
-        JPanel panelAux = new JPanel(); panelAux.setBounds(0,410,800,40);        ta = new JTextArea(30,60);  ta.setEditable(false);   ta.setBackground(UIManager.getColor(Color.darkGray));  ta.setFont(new Font("LucidaSans", Font.PLAIN, 20));
-            tf_message = new JTextField();         tf_message.setPreferredSize(new Dimension(650,30)); tf_message.setFont(new Font("LucidaSans", Font.PLAIN, 20));
+        ta = new JTextArea(30,60); ta.setBounds(0,0,400,410); ta.setEditable(false); ta.setBackground(UIManager.getColor("windowText")); ta.setForeground(Color.WHITE); ta.setFont(new Font("LucidaSans", Font.PLAIN, 20));
+        JPanel panelAux = new JPanel(); panelAux.setBounds(0,410,800,40);   //panelAux.setBackground(UIManager.getColor("CheckBoxMenuItem.acceleratorForeground"));
+            tf_message = new JTextField();         tf_message.setPreferredSize(new Dimension(650,30)); tf_message.setFont(new Font("LucidaSans", Font.PLAIN, 20)); //tf_message.setBorder(UIManager.getBorder("RootPane.plainDialogBorder"));
             btn_anexo = new JLabel(imageIcon);     btn_anexo.setPreferredSize(new Dimension(30,30));
             btn_send = new JLabel(imageIcon2);     btn_send.setPreferredSize(new Dimension(50,30));
         panelAux.add(btn_anexo, BorderLayout.BEFORE_LINE_BEGINS);  panelAux.add(tf_message, BorderLayout.CENTER);   panelAux.add(btn_send, BorderLayout.LINE_END);
-        chatFrame.add(panelAux, BorderLayout.AFTER_LAST_LINE); chatFrame.getContentPane().add(ta);
+        chatFrame.add(panelAux, BorderLayout.AFTER_LAST_LINE); chatFrame.getContentPane().add(ta, BorderLayout.WEST);
         chatFrame.setVisible(true); chatFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
@@ -106,11 +101,17 @@ public class Chat {
                     System.out.println("FILE: Read " + numRead + " bytes."+ new String(newData, StandardCharsets.US_ASCII));
                     fileReceiving=true;
                     listaFile.add(newData);
-                    if(isEndOfFile(newData)) { fileReceiving=false;
-                        try { saveReceivedFile(); } catch (IOException e) { e.printStackTrace(); }
-                    }  }
 
-                else if(fileReceiving == false){
+                    if(isEndOfFile(newData)) {
+                        fileReceiving=false;
+                        try { saveReceivedFile(); } catch (IOException e) { e.printStackTrace(); }
+                    } else if (isEndOfImage(newData)) {
+                        fileReceiving = false;
+                        try {   saveReceivedImage();    } catch (IOException e) { e.printStackTrace(); }
+                    }
+                }
+
+                else if(!fileReceiving){
                     System.out.println("Read "+new String(newData, StandardCharsets.US_ASCII));
                     String mensagemRecebida = new String(newData, StandardCharsets.US_ASCII);
                     ta.append("\nPC2: "+mensagemRecebida.substring(13,mensagemRecebida.length()-1));
@@ -178,6 +179,9 @@ public class Chat {
     public boolean isEndOfFile(byte[] dataReceived){
         return new String(dataReceived, StandardCharsets.US_ASCII).contains("Ends");
     }
+    public boolean isEndOfImage(byte[] dataReceived){
+        return new String(dataReceived, StandardCharsets.US_ASCII).contains("IEND");
+    }
     public void saveReceivedFile() throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();   x++;
         for(byte[] b: listaFile){
@@ -208,5 +212,23 @@ public class Chat {
         Files.write(Paths.get(filePath), s.getBytes());
         listaFile.clear();
 
+    }
+    public void saveReceivedImage() throws IOException {
+        x++;
+        System.out.println("SAVE IMAGE \n\n");
+        String filePaths = userDirectory+"/received"+x+".png";
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        for(byte[] b: listaFile){
+            System.out.println(Arrays.toString(b));
+            baos.write(b);
+        }
+        System.out.println("TAMANHO: "+baos.size());
+        List<Byte> lista = new ArrayList<>();
+        byte[] bytesToSave = baos.toByteArray();
+
+        Files.deleteIfExists(Paths.get(filePaths)); Files.createFile(Paths.get(filePaths));
+        Files.write(Paths.get(filePaths), bytesToSave);
+        ta.append("\nFicheiro recebido!");
+        listaFile.clear();
     }
 }
